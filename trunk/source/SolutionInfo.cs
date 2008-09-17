dab70abf7cc7b0061f3fff8915cbb3ac5ba1c1f0
agross@therightstuff.de <agross@therightstuff.de@9d7c3ade-840f-11dd-90a9-6306d3ffb32f>
2008-09-17 10:52:53 +0000

    Initial layout

---
+++ trunk/source/SolutionInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyCompany("Alexander Groß and Marnix van Valen")]
+[assembly: AssemblyProduct("NAnt Extensions")]
+[assembly: AssemblyCopyright("Copyright © Alexander Groß and Marnix van Valen 2008")]
