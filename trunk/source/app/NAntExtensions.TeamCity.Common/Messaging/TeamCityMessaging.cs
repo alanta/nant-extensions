534f22ef5aadc5cbd67e7913b26ffb668e77d75c
agross@therightstuff.de <agross@therightstuff.de@9d7c3ade-840f-11dd-90a9-6306d3ffb32f>
2008-09-18 20:29:57 +0000

    string.Format -> String.Format

--- trunk/source/app/NAntExtensions.TeamCity.Common/Messaging/TeamCityMessaging.cs
+++ trunk/source/app/NAntExtensions.TeamCity.Common/Messaging/TeamCityMessaging.cs
@@ -19,32 +19,31 @@ namespace NAntExtensions.TeamCity.Common.Messaging
 			_writer = writer;
 		}
 
-
 		#region Test messages
 		public void TestSuiteStarted(string assemblyName)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testSuiteStarted name='{0}']",
 			                                FormatValue(assemblyName)));
 		}
 
 		public void TestSuiteFinished(string assemblyName)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testSuiteFinished name='{0}']",
 			                                FormatValue(assemblyName)));
 		}
 
 		public void TestStarted(string testName)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testStarted name='{0}']",
 			                                FormatValue(testName)));
 		}
 
 		public void TestIgnored(string testName, string message)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testIgnored name='{0}' message='{1}']",
 			                                FormatValue(testName),
 			                                FormatValue(message)));
@@ -56,7 +55,7 @@ namespace NAntExtensions.TeamCity.Common.Messaging
 			FormatException(exception, formattedException);
 			FormatValue(formattedException);
 
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testFailed  name='{0}' message='{1}' details='{2}' type='{3}']",
 			                                FormatValue(testName),
 			                                FormatValue(exception.Message),
@@ -66,7 +65,7 @@ namespace NAntExtensions.TeamCity.Common.Messaging
 
 		public void TestOutputStream(string testName, string outputStream)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testStdOut name='{0}' out='{1}']",
 			                                FormatValue(testName),
 			                                FormatValue(outputStream)));
@@ -74,7 +73,7 @@ namespace NAntExtensions.TeamCity.Common.Messaging
 
 		public void TestErrorStream(string testName, string errorStream)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testStdErr name='{0}' out='{1}']",
 			                                FormatValue(testName),
 			                                FormatValue(errorStream)));
@@ -82,39 +81,39 @@ namespace NAntExtensions.TeamCity.Common.Messaging
 
 		public void TestFinished(string testName)
 		{
-			_writer.WriteLine(string.Format(CultureInfo.InvariantCulture,
+			_writer.WriteLine(String.Format(CultureInfo.InvariantCulture,
 			                                "##teamcity[testFinished name='{0}']",
 			                                FormatValue(testName)));
 		}
 		#endregion
 
-		static void FormatException(Exception exception, StringBuilder sb)
+		static void FormatException(Exception exception, StringBuilder builder)
 		{
 			if (exception == null)
 			{
 				return;
 			}
 
-			if (sb.Length > 0)
+			if (builder.Length > 0)
 			{
-				sb.AppendLine("--------------------------------");
+				builder.AppendLine("--------------------------------");
 			}
 
-			sb.AppendFormat("{0}: {1}", exception.GetType(), exception.Message);
-			sb.AppendLine();
+			builder.AppendFormat("{0}: {1}", exception.GetType(), exception.Message);
+			builder.AppendLine();
 
 			if (!String.IsNullOrEmpty(exception.Source))
 			{
-				sb.AppendFormat(" Source: {0}", exception.Source);
+				builder.AppendFormat(" Source: {0}", exception.Source);
 			}
-			sb.AppendLine();
+			builder.AppendLine();
 
-			sb.AppendFormat(" Stack: {0}", exception.StackTrace);
-			sb.AppendLine();
+			builder.AppendFormat(" Stack: {0}", exception.StackTrace);
+			builder.AppendLine();
 
 			if (exception.InnerException != null)
 			{
-				FormatException(exception.InnerException, sb);
+				FormatException(exception.InnerException, builder);
 			}
 		}
 
