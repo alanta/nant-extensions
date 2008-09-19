504c1882a35c27bc426506fcb06eb669b71c9c62
agross@therightstuff.de <agross@therightstuff.de@9d7c3ade-840f-11dd-90a9-6306d3ffb32f>
2008-09-19 20:28:29 +0000

    Fixing missing nunit.framework.dll in MSpec deployment artifact Adding unit tests for BuildEnvironment

---
+++ trunk/source/test/NAntExtensions.TeamCity.Common.Tests/When_the_build_environment_is_inspected_if_the_build_is_a_TeamCity_build.cs
@@ -0,0 +1,31 @@
+using System;
+
+using MbUnit.Framework;
+
+using NAntExtensions.ForTesting;
+
+namespace NAntExtensions.TeamCity.Common.Tests
+{
+	public class When_the_build_environment_is_inspected_if_the_build_is_a_TeamCity_build : Spec
+	{
+		const string TeamCityEnvironmentVariable = "TEAMCITY_PROJECT_NAME";
+
+		protected override void After_each_spec()
+		{
+			Environment.SetEnvironmentVariable(TeamCityEnvironmentVariable, null, EnvironmentVariableTarget.Process);
+		}
+
+		[Test]
+		public void Should_report_a_normal_build_if_TeamCity_environment_variable_does_not_exist()
+		{
+			Assert.IsFalse(BuildEnvironment.IsTeamCityBuild);
+		}
+
+		[Test]
+		public void Should_report_a_TeamCity_build_if_TeamCity_environment_variable_does_exist()
+		{
+			Environment.SetEnvironmentVariable(TeamCityEnvironmentVariable, "42", EnvironmentVariableTarget.Process);
+			Assert.IsTrue(BuildEnvironment.IsTeamCityBuild);
+		}
+	}
+}
