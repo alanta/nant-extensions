d2a4391daf10bb1a6df8522771f443e883275cd3
agross@therightstuff.de <agross@therightstuff.de@9d7c3ade-840f-11dd-90a9-6306d3ffb32f>
2008-09-27 18:01:00 +0000

    MSpec counters: Converting static properties to const fields

--- trunk/source/app/NAntExtensions.Machine.Specifications/Types/Counter.cs
+++ trunk/source/app/NAntExtensions.Machine.Specifications/Types/Counter.cs
@@ -6,36 +6,25 @@ namespace NAntExtensions.Machine.Specifications.Types
 {
 	public class Counter
 	{
-		public static string Contexts
-		{
-			get { return "mspec.contexts"; }
-		}
-
-		public static string Specifications
-		{
-			get { return "mspec.specs"; }
-		}
-
-		public static string FailedSpecifications
-		{
-			get { return "mspec.failedspecs"; }
-		}
+		public const string Contexts = "mspec.contexts";
+		public const string FailedSpecifications = "mspec.failedspecs";
+		public const string Specifications = "mspec.specs";
 
 		public static IEnumerable<string> All
 		{
 			get
 			{
-				var foo =
-					from prop in typeof(Counter).GetProperties(BindingFlags.Public | BindingFlags.Static | BindingFlags.GetProperty)
-					where typeof(string).IsAssignableFrom(prop.PropertyType)
+				var values =
+					from field in typeof(Counter).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField)
+					where typeof(string).IsAssignableFrom(field.FieldType)
 					select
-						typeof(Counter).InvokeMember(prop.Name,
-						                             BindingFlags.GetProperty | BindingFlags.Static | BindingFlags.Public,
+						typeof(Counter).InvokeMember(field.Name,
+						                             BindingFlags.GetField | BindingFlags.Static | BindingFlags.Public,
 						                             null,
 						                             null,
 						                             null).ToString();
 
-				return foo;
+				return values;
 			}
 		}
 	}
