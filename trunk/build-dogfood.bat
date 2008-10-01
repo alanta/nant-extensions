014260c2f1ff5dad74dd1e73d2c12fa472acfa93
agross@therightstuff.de <agross@therightstuff.de@9d7c3ade-840f-11dd-90a9-6306d3ffb32f>
2008-10-01 01:02:03 +0000

    Fixing dogfooding batch file: Documentation is now excluded

--- trunk/build-dogfood.bat
+++ trunk/build-dogfood.bat
@@ -1,14 +1,17 @@
 @echo off
+setlocal EnableDelayedExpansion
 cls
 
-tools\NAnt\NAnt.exe -buildfile:default.build %1
+rem tools\NAnt\NAnt.exe -buildfile:default.build %1
 
 if %errorlevel% EQU 0 (
 	for /r deploy %%f in (*.zip) do (
-		echo %%f
+		echo %%~nf | find /i "doc"
 
-		tools\Info-ZIP\unzip.exe -o "%%f" * -x License*.txt -d tools\NAnt
-		echo.
+		if errorlevel 1 (
+			tools\Info-ZIP\unzip.exe -o "%%f" * -x License*.txt -d tools\NAnt
+			echo.
+		)
 	)
 )
 
