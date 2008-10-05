ad1203fa6a965dfa204eda955b5f2041680b49f9
agross@therightstuff.de <agross@therightstuff.de@9d7c3ade-840f-11dd-90a9-6306d3ffb32f>
2008-10-05 00:10:27 +0000

    Adding interface members for new TeamCity 4.x features

--- trunk/source/app/NAntExtensions.TeamCity.Common/Messaging/ITeamCityMessageProvider.cs
+++ trunk/source/app/NAntExtensions.TeamCity.Common/Messaging/ITeamCityMessageProvider.cs
@@ -14,6 +14,17 @@ namespace NAntExtensions.TeamCity.Common.Messaging
 		void TestErrorStream(string testName, string errorStream);
 		void TestFinished(string testName);
 		void TestFinished(string testName, TimeSpan duration);
+
+		void BuildNumber(string newBuildNumber);
+		void BuildStatus(string newBuildNumber);
+		void PublishBuildArtifacts(string pathToArtifacts);
+		
+		void ProgressStart(string message);
+		void ProgressMessage(string message);
+		void ProgressFinished(string message);
+		
+		void ImportData(string type, string pathToDataFile);
+
 		void SendMessage(string message, params object[] parameters);
 	}
 }
